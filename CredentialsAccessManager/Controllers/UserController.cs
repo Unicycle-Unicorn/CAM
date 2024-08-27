5a84a6b587829d92866b831d0ab5123203daf9fa
UnicycleUnicorn <bradenfleming0801@gmail.com>
2024-08-27 08:54:26 -0400

    Creation of client details object

--- CredentialsAccessManager/Controllers/UserController.cs
+++ CredentialsAccessManager/Controllers/UserController.cs
@@ -73,14 +73,8 @@ public class UserController(ILogger<UserController> logger, ICamInterface camInt
     [Auth<CredentialAuth>(Permission.LOGIN)]
     public IActionResult Login([FromAuth<AuthUserId>] Guid userId, [FromAuth<AuthType>] string type, [FromAuth<AuthUsername>] string username)
     {
-	_ = HeaderUtils.TryGetHeader(HttpContext.Request, "Host", out string? ipAddr);
-	Logger.LogInformation($"Host: {ipAddr}");
-
-	_ = HeaderUtils.TryGetHeader(HttpContext.Request, "X-Real-IP", out string? remote);
-	Logger.LogInformation($"Real Ip: {remote}");
-
-	_ = HeaderUtils.TryGetHeader(HttpContext.Request, "User-Agent", out string? userAgent);
-	Logger.LogInformation($"User Agent: {userAgent}");
+	var clientDetails = ClientDetails.FromHttpContext(HttpContext);
+	Logger.LogInformation($"Client Details: {clientDetails}");
 
 	string clientIpAddress = "192.168.0.0";
         string sessionId = CredentialStore.CreateNewSession(userId, clientIpAddress).Output;
